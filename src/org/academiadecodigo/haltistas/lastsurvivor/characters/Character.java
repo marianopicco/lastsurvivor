3ecdf1967de5cb95af5401fecf90a2a54365b1fa
marianopicco <marianopicco@gmail.com>
2018-02-15 21:52:30 +0000

    Character class, declared basic properties, implemented basic methods

--- src/org/academiadecodigo/haltistas/lastsurvivor/characters/Character.java
+++ src/org/academiadecodigo/haltistas/lastsurvivor/characters/Character.java
@@ -2,11 +2,52 @@ package org.academiadecodigo.haltistas.lastsurvivor.characters;
 
 import org.academiadecodigo.haltistas.lastsurvivor.interfaces.Drawable;
 
-public class Character implements Drawable{
+public class Character implements Drawable {
+
+    private int hp;
+    private int baseAttack;
+    private boolean isAlive;
+
+    public Character(int hp, int baseAttack) {
+        this.hp = hp;
+        this.baseAttack = baseAttack;
+
+    }
+
+    public void attack(Character target){
+
+        target.getHit(baseAttack);
+
+    }
+
+    private void getHit(int damage) {
+
+        hp -= hp - damage;
+
+        if ( hp <= 0 ) {
+            this.toggleAlive();
+        }
+
+    }
+
+    public void toggleAlive(){
+        isAlive = !isAlive;
+
+    }
 
     @Override
     public void draw() {
 
     }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+
 
 }

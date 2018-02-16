234ea985823b97c0490dc13972df82be1ad4ee1a
Pedro Catana <pedrocatana@sapo.pt>
2018-02-16 18:00:59 +0000

    create new classes to define diferent types of menus, subclasses of menu

---
+++ src/org/academiadecodigo/haltistas/lastsurvivor/graphics/ActionMenu.java
@@ -0,0 +1,4 @@
+package org.academiadecodigo.haltistas.lastsurvivor.graphics;
+
+public class ActionMenu extends Menu{
+}
---
+++ src/org/academiadecodigo/haltistas/lastsurvivor/graphics/StatusMenu.java
@@ -0,0 +1,5 @@
+package org.academiadecodigo.haltistas.lastsurvivor.graphics;
+
+public class StatusMenu  extends Menu{
+
+}

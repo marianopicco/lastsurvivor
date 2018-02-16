1431e138c1e442c350b0975d384f73c4defac7bd
Pedro Catana <pedrocatana@sapo.pt>
2018-02-16 20:01:27 +0000

    draw Text in ActionMenu - in tests

--- src/org/academiadecodigo/haltistas/lastsurvivor/graphics/Action.java
+++ src/org/academiadecodigo/haltistas/lastsurvivor/graphics/Action.java
@@ -1,9 +1,19 @@
 package org.academiadecodigo.haltistas.lastsurvivor.graphics;
 
 public enum Action {
-    ATTACK,
-    MAGIC,
-    DEFEND,
-    ITEMS
+    ATTACK("ATTACK"),
+    MAGIC("MAGIC"),
+    DEFEND("DEFEND"),
+    ITEMS("ITEMS");
 
+    private String action;
+
+    Action (String action){
+        this.action=action;
+    }
+
+    public String getAction() {
+        return action;
+    }
 }
+
--- src/org/academiadecodigo/haltistas/lastsurvivor/graphics/menus/ActionMenu.java
+++ src/org/academiadecodigo/haltistas/lastsurvivor/graphics/menus/ActionMenu.java
@@ -1,15 +1,23 @@
 package org.academiadecodigo.haltistas.lastsurvivor.graphics.menus;
 
+import org.academiadecodigo.haltistas.lastsurvivor.graphics.Action;
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.graphics.Text;
 
 public class ActionMenu extends Menu {
+
 
     public ActionMenu() {
 
         Rectangle actionMenu = new Rectangle(menuX(), menuY(), menuWidth(), menuHeight());
         actionMenu.fill();
         actionMenu.setColor(Color.CYAN);
+    }
+
+    @Override
+    public void draw() {
+        defineAction();
     }
 
     @Override
@@ -32,5 +40,20 @@ public class ActionMenu extends Menu {
         return super.menuWidth() / 2;
     }
 
-    
+    public void defineAction() {
+
+        Text attack = new Text(260, menuY() + 10, Action.ATTACK.getAction());
+        Text magic = new Text(260, menuY() + 30, Action.MAGIC.getAction());
+        Text defend = new Text(260, menuY() + 50, Action.DEFEND.getAction());
+        Text item = new Text(260, menuY() + 70, Action.ITEMS.getAction());
+
+        attack.draw();
+        magic.draw();
+        defend.draw();
+        item.draw();
+
+
+    }
+
+
 }

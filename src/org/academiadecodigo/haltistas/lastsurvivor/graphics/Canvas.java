4b892e47aba153629f1ae49d9ea22a8a9d8941e8
Pedro Catana <pedrocatana@sapo.pt>
2018-02-16 14:49:14 +0000

    added directory assets with background test and create background

--- src/org/academiadecodigo/haltistas/lastsurvivor/graphics/Canvas.java
+++ src/org/academiadecodigo/haltistas/lastsurvivor/graphics/Canvas.java
@@ -2,15 +2,29 @@ package org.academiadecodigo.haltistas.lastsurvivor.graphics;
 
 import org.academiadecodigo.haltistas.lastsurvivor.interfaces.Drawable;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Canvas implements Drawable {
 
     @Override
     public void draw(){
 
+        createAreaOfGame();
+createBackground();
+
+    }
+
+    private void createAreaOfGame() {
         Rectangle areaOfGame = new Rectangle(10, 10, 1000, 700);
 
         areaOfGame.draw();
+    }
+
+    private void createBackground() {
+        
+        Picture pic = new Picture(-150, -180, "assets/background_test.jpg");
+        pic.grow(-2000, -180);
+        pic.draw();
     }
 
 }

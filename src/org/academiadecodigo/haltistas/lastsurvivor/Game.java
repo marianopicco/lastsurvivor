86663797c1928366c5cbb2f1b8479fce694b32c8
marianopicco <marianopicco@gmail.com>
2018-02-22 02:58:02 +0000

    Removed thread sleeps between attacks and menu iteration

--- src/org/academiadecodigo/haltistas/lastsurvivor/Game.java
+++ src/org/academiadecodigo/haltistas/lastsurvivor/Game.java
@@ -72,13 +72,11 @@ public class Game {
                 }
 
                 receivedMenuChoice = true;
-                Thread.sleep(500);
 
                 keyPressed = null;
 
             }
 
-            Thread.sleep(500);
         }
     }
 
@@ -136,12 +134,6 @@ public class Game {
     private void enemyTurn() {
 
         for (Character enemy : currentStage.getEnemies()) {
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
 
             try {
                 canvas.translateCharacter(canvas.getEvilGuy(),canvas.getGoodGuy());
